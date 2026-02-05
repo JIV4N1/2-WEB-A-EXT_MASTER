fc150c83e38d9a3fc3347fee5cd42208043746d2
Josué Ginez <ginezivan409@gmail.com>
2026-02-04 20:48:58 -0600

    Clase 1, Actividad 1

---
+++ clase_1/tiposde_datos.js
@@ -0,0 +1,35 @@
+// Tipos datos
+
+"Algo de codigo"; /* Tambien se puede comentar así en JS */
+
+// Strings
+// Entre comlillas
+"Hola soy un string";
+"Yo tambien soy string";
+
+// Numbers
+2;
+1.1 - 1;
+
+//Booleans
+true;
+false;
+
+// Null
+null;
+
+// Undefined
+undefined;
+
+console.log(typeof "Esto es un string");
+
+// Ejemplos:
+console.log(typeof 42);
+console.log(typeof "Veinticinco");
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof "");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof FALSE);
