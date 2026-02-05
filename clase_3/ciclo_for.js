b3c05cc349e2182ca445636f57357af3d1d0e943
Josué Ginez <ginezivan409@gmail.com>
2026-02-04 21:21:30 -0600

    Apuntes clase 3

---
+++ clase_3/ciclo_for.js
@@ -0,0 +1,13 @@
+// Imprimir 5 veces mi nombre
+
+// i++; => i += 1; => i = i + 1;
+
+for (let i = 0; i < 5; i++) {
+  console.log("Josué");
+}
+
+// Podemos modificar el contador para que vaya de dos en dos
+
+for (let i = 0; i < 20; i += 2) {
+  console.log(i);
+}
